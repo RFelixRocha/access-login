28bf0dd346b22502dc63f43f6f35d7b9413a3415
Raimundo Felix da Rocha <felixrochaeso@gmail.com>
2022-02-13 17:52:14 -0400

    Implement logout

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../contexts/auth'
 
 const Home = () => {
-  return <h1>Home pages</h1>
+  const { logout, authenticated } = useContext(AuthContext)
+
+  const handleLogout = () => {
+    logout()
+  }
+
+  return (
+    <>
+    <h2>Home pages</h2>
+    <p>{String(authenticated) }</p>
+    <button onClick={  handleLogout }>Logout</button>
+    </>
+  )
 }
 
 export { Home }
