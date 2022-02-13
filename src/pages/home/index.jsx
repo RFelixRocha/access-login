396eb9dcf44627414f00874df813de1bc1ca3043
Raimundo Felix da Rocha <felixrochaeso@gmail.com>
2022-02-13 19:02:28 -0400

    List of users

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,18 +1,41 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../../contexts/auth'
+import { getUsers } from '../../contexts/authProvider'
 
 const Home = () => {
-  const { logout, authenticated } = useContext(AuthContext)
+  const { logout } = useContext(AuthContext)
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    ( async () => {
+      const response = await getUsers()
+      setUsers(response.data)
+      setLoading(false)
+    })()
+  },[])
 
   const handleLogout = () => {
     logout()
   }
 
+  if (loading) {
+    return <div className='loading'> Carregando Informações...</div>
+  }
+
   return (
     <>
     <h2>Home pages</h2>
-    <p>{String(authenticated) }</p>
     <button onClick={  handleLogout }>Logout</button>
+    <ul>
+      {
+        users.map( (user) => (
+          <li key={ user.id }>
+            { user.id } - { user.first_name } - { user.email }
+          </li>
+        ))
+      }
+    </ul>
     </>
   )
 }
